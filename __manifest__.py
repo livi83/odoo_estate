35321182efdfd30d62113ad7e0dab2cb5b713d0f
livi83 <livia.kelebercova@gmail.com>
2024-09-11 20:59:01 +0200

    Modified manifest

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'estate',
+    'version': '1.0',
+    'summary': 'A brief description of the module',
+    'description': 'A detailed description of the module',
+    'author': 'Lívia Kelebercová',
+    'website': 'https://symetra.sk',
+    'category': 'Category Name',
+    'depends': ['base'],
+    'data': [
+        
+    ],
+    'installable': True,
+    'auto_install': False,
+}
